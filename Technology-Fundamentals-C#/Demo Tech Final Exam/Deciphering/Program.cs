337856ac097136df1bcce3f99d462342d8c1bdda
ITonev <ivan_tonev@mail.bg>
2019-04-08 17:50:51 +0300

    added second task

---
+++ Technology-Fundamentals-C#/Demo Tech Final Exam/Deciphering/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Deciphering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encryptedString = Console.ReadLine();
+            var substrings = Console.ReadLine().Split();
+
+            var pattern = @"^[d-z{}|#]*$";
+            
+            if (Regex.IsMatch(encryptedString, pattern))
+            {
+                var decryptedString = encryptedString.Select(x => x = (char)(x - 3));
+
+                var result = string.Empty;
+
+                foreach (var @char in decryptedString)
+                {
+                    result += @char;
+                }
+
+                result = result.Replace(substrings[0], substrings[1]);
+                Console.WriteLine(result);
+            }
+
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+        }
+    }
+}
