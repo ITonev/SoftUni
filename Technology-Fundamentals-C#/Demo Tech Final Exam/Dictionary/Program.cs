776b31552e6a9badeb81b632a79e57986be3e8f3
ITonev <ivan_tonev@mail.bg>
2019-04-08 16:39:45 +0300

    added first task

--- Technology-Fundamentals-C#/Demo Tech Final Exam/Dictionary/Program.cs
+++ Technology-Fundamentals-C#/Demo Tech Final Exam/Dictionary/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Dictionary
 {
@@ -6,7 +8,49 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = Console.ReadLine();
+
+            var dictionary = new Dictionary<string, List<string>>();
+
+            var wordAndDefinitionPair = input.Split(" | ").ToList();
+
+            for (int i = 0; i < wordAndDefinitionPair.Count; i++)
+            {
+                var currentpair = wordAndDefinitionPair[i].Split(": ");
+                var word = currentpair[0];
+                var definition = currentpair[1];
+
+                if (!dictionary.ContainsKey(word))
+                {
+                    dictionary[word] = new List<string>();
+                }
+
+                dictionary[word].Add(definition);
+            }
+
+            var wordsToprint = Console.ReadLine().Split(" | ");
+
+            foreach (var word in wordsToprint)
+            {
+                if (dictionary.ContainsKey(word))
+                {
+                    var DefList = dictionary[word].OrderByDescending(x=>x.Count()).ToList();
+
+                    Console.WriteLine($"{word}");
+
+                    foreach (var definition in DefList)
+                    {
+                        Console.WriteLine($"-{definition}");
+                    }
+                }
+            }
+
+            var command = Console.ReadLine();
+
+            if (command=="List")
+            {
+                Console.WriteLine(string.Join(" ", dictionary.Keys.OrderBy(x=>x)));
+            }
         }
     }
 }
