a8c2061b19c625f738eae4b0f1ca78a063d01140
ITonev <ivan_tonev@mail.bg>
2019-05-22 11:38:39 +0300

    added associative arrays problems

--- C#-Advanced/Sets-and-Dictionaries-Advanced/Exercise/Wardrobe/Program.cs
+++ C#-Advanced/Sets-and-Dictionaries-Advanced/Exercise/Wardrobe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Wardrobe
 {
@@ -6,8 +7,58 @@ namespace Wardrobe
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>(); // color -> clothes-> count
+
+            for (int i = 0; i < n; i++)
+            {
+                var entry = Console.ReadLine()
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                var color = entry[0];
+                var clothes = entry[1]
+                    .Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe[color] = new Dictionary<string, int>();
+                }
 
 
+                for (int j = 0; j < clothes.Length; j++)
+                {
+                    if (!wardrobe[color].ContainsKey(clothes[j]))
+                    {
+                        wardrobe[color][clothes[j]] = 0;
+                    }
+
+                    wardrobe[color][clothes[j]]++;
+                }
+            }
+
+            var command = Console.ReadLine().Split();
+            var colorToSearch = command[0];
+            var clothToSearch = command[1];
+
+            foreach (var kvp in wardrobe)
+            {
+                Console.WriteLine($"{kvp.Key} clothes:");
+                var clothes = kvp.Value;
+
+                foreach (var cloth in clothes)
+                {
+                    if (kvp.Key == colorToSearch && cloth.Key == clothToSearch)
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value} (found!)");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
+                    }
+                }
+            }
         }
     }
 }
