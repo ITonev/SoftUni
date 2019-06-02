e2eb33f04892c60d2bc984487f2ebfd23d1eba99
ITonev <ivan_tonev@mail.bg>
2019-06-02 11:41:06 +0300

    added functional programming lab problems

---
+++ C#-Advanced/Functional-Programming/Lab/Add VAT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(n => n = n * 1.2)
+                .ToList()
+                .ForEach(n => Console.WriteLine($"{n:f2}"));
+        }
+    }
+}
---
+++ C#-Advanced/Functional-Programming/Lab/Count Uppercase Words/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(upperCase)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+
+        public static Func<string, bool> upperCase = n => n[0] == n.ToUpper()[0];
+    }
+}
