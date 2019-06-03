43aa59af7bd61415df8151ea7a3f602a99fef738
ITonev <ivan_tonev@mail.bg>
2019-06-03 22:27:36 +0300

    added more func exercises

--- C#-Advanced/Functional-Programming/Exercise/Predicate Party!/Program.cs
+++ C#-Advanced/Functional-Programming/Exercise/Predicate Party!/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Predicate_Party_
 {
@@ -6,8 +7,69 @@ namespace Predicate_Party_
     {
         static void Main(string[] args)
         {
+            var people = Console.ReadLine().Split().ToList();
 
 
+            var command = Console.ReadLine();
+
+            while (command != "Party!")
+            {
+                var tokens = command.Split();
+                var secondCommand = tokens[1];
+
+                Func<string, bool> filter;
+
+                switch (tokens[0])
+                {
+                    case "Remove" when secondCommand == "StartsWith":
+                        filter = x => !x.StartsWith(tokens[2]);
+                        break;
+
+                    case "Remove" when secondCommand == "EndsWith":
+                        filter = x => !x.EndsWith(tokens[2]);
+                        break;
+
+                    case "Remove" when secondCommand == "Lenght":
+                        filter = x => x.Length != int.Parse(tokens[2]);
+                        break;
+
+                    case "Double" when secondCommand == "StartsWith":
+                        filter = x => x.StartsWith(tokens[2]);
+                        break;
+
+                    case "Double" when secondCommand == "EndsWith":
+                        filter = x => x.EndsWith(tokens[2]);
+                        break;
+
+                    default: //"Double" when secondCommand == "Lenght":
+                        filter = x => x.Length == int.Parse(tokens[2]);
+                        break;
+
+                }
+
+                if (tokens[0] == "Remove")
+                {
+                    people = people.Where(filter).ToList();
+                }
+
+                else if (tokens[0] == "Double")
+                {
+                    var tempList = people.Where(filter).ToList();
+
+                    foreach (var guest in tempList)
+                    {
+                        var guestIndex = people.IndexOf(guest);
+                        people.Insert(guestIndex, guest);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(people.Any()
+                ? $"{string.Join(", ", people)} are going to the party!"
+                : "Nobody is going to the party!");
         }
     }
 }
+
---
+++ C#-Advanced/Functional-Programming/Exercise/TriFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split();
+
+            Func<string, int, bool> isValidWord = (x, num) => x.ToCharArray()
+                .Select(ch => (int)ch).Sum() >= num;
+
+            Func<string[], int, Func<string, int, bool>, string> validName = (arr, num, func) => arr
+                .FirstOrDefault(x => func(x, num));
+
+            var result = validName(names, number, isValidWord);
+
+            Console.WriteLine(result);
+        }
+    }
+}
