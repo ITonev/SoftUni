a1341474967eeb5200c2739170fdbb300f98f1cf
ITonev <ivan_tonev@mail.bg>
2019-06-03 21:22:55 +0300

    added func exercises problems

---
+++ C#-Advanced/Functional-Programming/Exercise/List Of Predicates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = int.Parse(Console.ReadLine());
+
+            var dividers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Queue<int> numbers = new Queue<int>();
+
+            Func<int, int, bool> Func = (x, y) => x % y != 0;
+
+            for (int i = 1; i <= end; i++)
+            {
+                bool isValid = true;
+
+                foreach (var num in dividers)
+                {
+                    if (Func(i, num))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (isValid)
+                {
+                    numbers.Enqueue(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ C#-Advanced/Functional-Programming/Exercise/Predicate Party!/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Predicate_Party_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
