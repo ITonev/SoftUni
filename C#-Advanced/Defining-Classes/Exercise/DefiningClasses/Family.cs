f14c7db5068d420ac2f593b08053664159be005a
ITonev <ivan_tonev@mail.bg>
2019-06-06 10:25:19 +0300

    added classes exercise problems

---
+++ C#-Advanced/Defining-Classes/Exercise/DefiningClasses/Family.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> People { get; set; }
+
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            int highestAge = int.MinValue;
+
+            foreach (var person in this.People)
+            {
+                if (person.Age>highestAge)
+                {
+                    highestAge = person.Age;
+                }
+            }
+
+            return this.People.Find(x=>x.Age==highestAge);
+        }
+    }
+}
---
+++ C#-Advanced/Defining-Classes/Exercise/DefiningClasses/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currrentPerson = Console.ReadLine().Split();
+                Person person = new Person(currrentPerson[0], int.Parse(currrentPerson[1]));
+
+                family.AddMember(person);
+            }
+
+            var oldest = family.GetOldestMember();
+
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
+        }
+    }
+}
