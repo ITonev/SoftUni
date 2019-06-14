b741c96d8fa51613f81927263e392dbff59cb2c9
ITonev <ivan_tonev@mail.bg>
2019-06-14 14:57:38 +0300

    generics exercises

---
+++ C#-Advanced/Generics/Exercise/Tuple/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var personInfo = Console.ReadLine().Split();
+            var name = personInfo[0] + " " + personInfo[1];
+            var address = personInfo[2];
+
+            var beerInfo = Console.ReadLine().Split();
+            string secondName = beerInfo[0];
+            int beers = int.Parse(beerInfo[1]);
+            
+            var intDoubleDuo = Console.ReadLine().Split();
+            int firstNum = int.Parse(intDoubleDuo[0]);
+            double secondNum = double.Parse(intDoubleDuo[1]);
+
+            Tuple<string, string> infoPerson = new Tuple<string, string>(name, address);
+            Tuple<string, int> infoBeer = new Tuple<string, int>(secondName, beers);
+            Tuple<int,double> infoNumbers = new Tuple<int,double>(firstNum, secondNum);
+
+            Console.WriteLine(infoPerson.ToString());
+            Console.WriteLine(infoBeer.ToString());
+            Console.WriteLine(infoNumbers.ToString());
+        }
+    }
+}
---
+++ C#-Advanced/Generics/Exercise/Tuple/Tuple.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Tuple<T,V>
+    {
+        private T item1;
+        private V item2;
+
+        public T Item1
+        {
+            get
+            {
+                return this.item1;
+            }
+            private set
+            {
+                this.item1 = value;
+            }
+        }
+
+        public V Item2
+        {
+            get
+            {
+                return this.item2;
+            }
+            private set
+            {
+                this.item2 = value;
+            }
+        }
+
+        public Tuple(T item1, V item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2}";
+        }
+    }
+}
