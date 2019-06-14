64ce671ecd9fb076cb4e38d81f0e222d3a65030a
ITonev <ivan_tonev@mail.bg>
2019-06-14 15:25:24 +0300

    added last generic problem

---
+++ C#-Advanced/Generics/Exercise/Threeuple/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Threeuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var personInfo = Console.ReadLine().Split();
+            var name = personInfo[0] + " " + personInfo[1];
+            var address = personInfo[2];
+            var town = string.Empty;
+
+            if (personInfo.Length > 4)
+            {
+                town = personInfo[3] + " " + personInfo[4];
+            }
+            else
+            {
+                town = personInfo[3];
+            }
+
+            var beerInfo = Console.ReadLine().Split();
+            var name2 = beerInfo[0];
+            var liters = int.Parse(beerInfo[1]);
+            var drunk = beerInfo[2] == "drunk" ? true : false;
+
+            var bankInfo = Console.ReadLine().Split();
+            var personName = bankInfo[0];
+            var balance = double.Parse(bankInfo[1]);
+            var bankName = bankInfo[2];
+
+            Threeuple<string, string, string> infoPerson = new Threeuple<string, string, string>(name, address, town);
+            Threeuple<string, int, bool> infoBeer = new Threeuple<string, int, bool>(name2, liters, drunk);
+            Threeuple<string, double, string> infoBank = new Threeuple<string, double, string>(personName, balance, bankName);
+
+            Console.WriteLine(infoPerson.GetInfo());
+            Console.WriteLine(infoBeer.GetInfo());
+            Console.WriteLine(infoBank.GetInfo());
+
+        }
+    }
+}
---
+++ C#-Advanced/Generics/Exercise/Threeuple/Threeuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threeuple
+{
+    public class Threeuple<TItem1, TItem2, TItem3>
+    {
+        private TItem1 Item1 { get; set; }
+        private TItem2 Item2 { get; set; }
+        private TItem3 Item3 { get; set; }
+
+        public Threeuple(TItem1 item1, TItem2 item2, TItem3 item3)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
+        }
+
+        public string GetInfo()
+        {
+            return $"{this.Item1} -> {this.Item2} -> {this.Item3}";
+
+        }
+    }
+}
