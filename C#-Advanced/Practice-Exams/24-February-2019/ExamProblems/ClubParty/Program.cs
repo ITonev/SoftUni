f38ab9b3cecd8eb9537b239d85d88553db0762ea
ITonev <ivan_tonev@mail.bg>
2019-06-15 12:58:47 +0300

    added 24th February exam problems

---
+++ C#-Advanced/Practice-Exams/24-February-2019/ExamProblems/ClubParty/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ClubParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = int.Parse(Console.ReadLine());
+
+            var input = Console.ReadLine().Split();
+
+            Stack<string> stack = new Stack<string>(input);
+
+            string regPattern = @"[a-zA-Z]";
+
+            Queue<string> hall = new Queue<string>();
+            List<int> hallReservations = new List<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var item = stack.Pop();
+
+                if (Regex.IsMatch(item, regPattern))
+                {
+                    hall.Enqueue(item);
+                }
+
+                else if (hall.Count > 0)
+                {
+                    int reservations = int.Parse(item);
+
+                    if (hallReservations.Sum() + reservations <= capacity)
+                    {
+                        hallReservations.Add(reservations);
+                    }
+
+                    else
+                    {
+                        var currentHall = hall.Dequeue();
+                        Console.WriteLine($"{currentHall} -> {string.Join(", ", hallReservations)}");
+                        hallReservations.Clear();
+
+                        if (hall.Count != 0)
+                        {
+                            hallReservations.Add(reservations);
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#-Advanced/Practice-Exams/24-February-2019/ExamProblems/TronRacers/Program.cs
@@ -0,0 +1,252 @@
+﻿using System;
+
+namespace TronRacers
+{
+    public class Program
+    {
+        static int firstPlayerRowPosition;
+        static int firstPlayerColPosition;
+        static int secondPlayerRowPosition;
+        static int secondPlayerColPosition;
+        static bool isAlive = true;
+
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                var currentCol = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < currentCol.Length; col++)
+                {
+                    matrix[row, col] = currentCol[col];
+                    if (currentCol[col] == 'f')
+                    {
+                        firstPlayerRowPosition = row;
+                        firstPlayerColPosition = col;
+                    }
+                    else if (currentCol[col] == 's')
+                    {
+                        secondPlayerRowPosition = row;
+                        secondPlayerColPosition = col;
+                    }
+                }
+            }
+
+            while (isAlive != false)
+            {
+                var input = Console.ReadLine().Split();
+
+                var firstPlayerCommand = input[0];
+                var secondPlayerCommand = input[1];
+
+                FirstPlayerMovement(matrix, firstPlayerCommand);
+
+                if (isAlive != false)
+                {
+                    SecondPlayerMovement(matrix, secondPlayerCommand);
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void SecondPlayerMovement(char[,] matrix, string command)
+        {
+            switch (command)
+            {
+                case "up":
+                    if (secondPlayerRowPosition - 1 < 0)
+                    {
+                        secondPlayerRowPosition = matrix.GetLength(0) - 1;
+                    }
+                    else
+                    {
+                        secondPlayerRowPosition--;
+                    }
+
+                    if (matrix[secondPlayerRowPosition, secondPlayerColPosition] == 'f')
+                    {
+                        matrix[secondPlayerRowPosition, secondPlayerColPosition] = 'x';
+                        isAlive = false;
+                    }
+                    else
+                    {
+                        matrix[secondPlayerRowPosition, secondPlayerColPosition] = 's';
+                    }
+                    break;
+
+                case "down":
+                    if (secondPlayerRowPosition + 1 > matrix.GetLength(0) - 1)
+                    {
+                        secondPlayerRowPosition = 0;
+                    }
+                    else
+                    {
+                        secondPlayerRowPosition++;
+                    }
+
+                    if (matrix[secondPlayerRowPosition, secondPlayerColPosition] == 'f')
+                    {
+                        matrix[secondPlayerRowPosition, secondPlayerColPosition] = 'x';
+                        isAlive = false;
+                    }
+                    else
+                    {
+                        matrix[secondPlayerRowPosition, secondPlayerColPosition] = 's';
+                    }
+
+                    break;
+
+                case "left":
+                    if (secondPlayerColPosition - 1 < 0)
+                    {
+                        secondPlayerColPosition = matrix.GetLength(1) - 1;
+                    }
+                    else
+                    {
+                        secondPlayerColPosition--;
+                    }
+
+                    if (matrix[secondPlayerRowPosition, secondPlayerColPosition] == 'f')
+                    {
+                        matrix[secondPlayerRowPosition, secondPlayerColPosition] = 'x';
+                        isAlive = false;
+                    }
+                    else
+                    {
+                        matrix[secondPlayerRowPosition, secondPlayerColPosition] = 's';
+                    }
+
+                    break;
+
+                case "right":
+                    if (secondPlayerColPosition + 1 > matrix.GetLength(1) - 1)
+                    {
+                        secondPlayerColPosition = 0;
+                    }
+                    else
+                    {
+                        secondPlayerColPosition++;
+                    }
+
+                    if (matrix[secondPlayerRowPosition, secondPlayerColPosition] == 'f')
+                    {
+                        matrix[secondPlayerRowPosition, secondPlayerColPosition] = 'x';
+                        isAlive = false;
+                    }
+                    else
+                    {
+                        matrix[secondPlayerRowPosition, secondPlayerColPosition] = 's';
+                    }
+
+                    break;
+            }
+
+        }
+
+        private static void FirstPlayerMovement(char[,] matrix, string command)
+        {
+            switch (command)
+            {
+                case "up":
+                    if (firstPlayerRowPosition - 1 < 0)
+                    {
+                        firstPlayerRowPosition = matrix.GetLength(0) - 1;
+                    }
+                    else
+                    {
+                        firstPlayerRowPosition--;
+                    }
+
+                    if (matrix[firstPlayerRowPosition, firstPlayerColPosition] == 's')
+                    {
+                        matrix[firstPlayerRowPosition, firstPlayerColPosition] = 'x';
+                        isAlive = false;
+                    }
+                    else
+                    {
+                        matrix[firstPlayerRowPosition, firstPlayerColPosition] = 'f';
+                    }
+                    break;
+
+                case "down":
+                    if (firstPlayerRowPosition + 1 > matrix.GetLength(0) - 1)
+                    {
+                        firstPlayerRowPosition = 0;
+                    }
+                    else
+                    {
+                        firstPlayerRowPosition++;
+                    }
+
+                    if (matrix[firstPlayerRowPosition, firstPlayerColPosition] == 's')
+                    {
+                        matrix[firstPlayerRowPosition, firstPlayerColPosition] = 'x';
+                        isAlive = false;
+                    }
+                    else
+                    {
+                        matrix[firstPlayerRowPosition, firstPlayerColPosition] = 'f';
+                    }
+
+                    break;
+
+                case "left":
+                    if (firstPlayerColPosition - 1 < 0)
+                    {
+                        firstPlayerColPosition = matrix.GetLength(1) - 1;
+                    }
+                    else
+                    {
+                        firstPlayerColPosition--;
+                    }
+
+                    if (matrix[firstPlayerRowPosition, firstPlayerColPosition] == 's')
+                    {
+                        matrix[firstPlayerRowPosition, firstPlayerColPosition] = 'x';
+                        isAlive = false;
+                    }
+                    else
+                    {
+                        matrix[firstPlayerRowPosition, firstPlayerColPosition] = 'f';
+                    }
+
+                    break;
+
+                case "right":
+                    if (firstPlayerColPosition + 1 > matrix.GetLength(1) - 1)
+                    {
+                        firstPlayerColPosition = 0;
+                    }
+                    else
+                    {
+                        firstPlayerColPosition++;
+                    }
+
+                    if (matrix[firstPlayerRowPosition, firstPlayerColPosition] == 's')
+                    {
+                        matrix[firstPlayerRowPosition, firstPlayerColPosition] = 'x';
+                        isAlive = false;
+                    }
+                    else
+                    {
+                        matrix[firstPlayerRowPosition, firstPlayerColPosition] = 'f';
+                    }
+
+                    break;
+            }
+        }
+    }
+}
