69e140568cc27acea79cb607de1df700dda16d63
ITonev <ivan_tonev@mail.bg>
2019-06-17 21:32:40 +0300

    added iterators exercise

---
+++ C#-Advanced/Iterators-And-Comparators/Exercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> elements;
+
+        private int index;
+
+        public ListyIterator()
+        {
+            this.elements = new List<T>();
+            this.index = 0;
+        }
+    }
+}
