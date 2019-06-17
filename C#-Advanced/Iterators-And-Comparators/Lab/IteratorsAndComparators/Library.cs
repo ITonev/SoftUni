b3a283785fe75bb91ec0cbe3bc51618bd9067503
ITonev <ivan_tonev@mail.bg>
2019-06-17 20:50:15 +0300

    added iterators and comparators

---
+++ C#-Advanced/Iterators-And-Comparators/Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books;
+
+        public int CurrentIndex { get; private set; }
+
+        public LibraryIterator(List<Book> books)
+        {
+            this.CurrentIndex = -1;
+            this.books = books;
+        }
+
+        public Book Current => this.books[CurrentIndex];
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            return ++CurrentIndex < this.books.Count;
+        }
+
+        public void Reset()
+        {
+
+        }
+    }
+}
