da03457936f821ac46b8426f6d01a8ac1ac017fe
ITonev <ivan_tonev@mail.bg>
2019-06-18 20:17:12 +0300

    added iterators exercise

--- C#-Advanced/Iterators-And-Comparators/Exercise/ListyIterator/ListyIterator.cs
+++ C#-Advanced/Iterators-And-Comparators/Exercise/ListyIterator/ListyIterator.cs
@@ -10,10 +10,48 @@ namespace ListyIterator
 
         private int index;
 
-        public ListyIterator()
+        public ListyIterator(params T[] elements)
         {
-            this.elements = new List<T>();
+            this.elements = new List<T>(elements);
             this.index = 0;
         }
+
+        public bool Move()
+        {
+            if (this.index + 1 < this.elements.Count)
+            {
+                this.index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            try
+            {
+                Console.WriteLine($"{this.elements[this.index]}");
+
+            }
+
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid Operation!");
+               // throw new ArgumentOutOfRangeException("Invalid Operation!");
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (this.index + 1 < this.elements.Count)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
     }
 }
--- C#-Advanced/Iterators-And-Comparators/Exercise/ListyIterator/Program.cs
+++ C#-Advanced/Iterators-And-Comparators/Exercise/ListyIterator/Program.cs
@@ -4,9 +4,42 @@ namespace ListyIterator
 {
     public class Program
     {
+        public static string[] tokens;
+        public static ListyIterator<string> listy;
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                var command = Console.ReadLine();
 
+                if (command == "END")
+                {
+                    break;
+                }
+
+                if (command.StartsWith("Create"))
+                {
+                    tokens = command.Substring(6).Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    listy = new ListyIterator<string>(tokens);
+                }
+
+                switch (command)
+                {
+                    case "Move":
+                        Console.WriteLine(listy.Move());
+                        break;
+
+                    case "HasNext":
+                        Console.WriteLine(listy.HasNext());
+                        break;
+
+                    case "Print":
+                        listy.Print();
+                        break;
+                }
+            }
         }
     }
 }
