dbaa389dcbde8b9260ed3a1ecfbcde04d1a2c1e7
ITonev <ivan_tonev@mail.bg>
2019-06-23 19:35:45 +0300

    added mid exam problems

---
+++ C#-Advanced/Exam-23-June-2019/ExamProblems/SpaceStationEstablishment/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+
+namespace SpaceStationEstablishment
+{
+    class Program
+    {
+        static int spaceshipRow;
+        static int spaceshipCol;
+        static int starPower;
+        static bool intoTheVold = false;
+
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] galaxy = new char[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                var cols = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < cols.Length; col++)
+                {
+                    galaxy[row, col] = cols[col];
+
+                    if (galaxy[row, col] == 'S')
+                    {
+                        spaceshipRow = row;
+                        spaceshipCol = col;
+                    }
+                }
+            }
+
+
+            while (starPower < 50)
+            {
+
+                var command = Console.ReadLine();
+
+                Movement(galaxy, command);
+
+                if (intoTheVold)
+                {
+                    Console.WriteLine("Bad news, the spaceship went to the void.");
+                    Console.WriteLine($"Star power collected: {starPower}");
+                    break;
+                }
+            }
+
+            if (starPower >= 50)
+            {
+                Console.WriteLine("Good news! Stephen succeeded in collecting enough star power!");
+                Console.WriteLine($"Star power collected: {starPower}");
+            }
+
+            for (int row = 0; row < galaxy.GetLength(0); row++)
+            {
+                for (int col = 0; col < galaxy.GetLength(0); col++)
+                {
+                    Console.Write(galaxy[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void Movement(char[,] galaxy, string command)
+        {
+            switch (command)
+            {
+                case "up":
+                    galaxy[spaceshipRow, spaceshipCol] = '-';
+                    if (BoundriesCheck(galaxy, spaceshipRow - 1, spaceshipCol))
+                    {
+                        spaceshipRow--;
+
+                        if (char.IsDigit(galaxy[spaceshipRow, spaceshipCol]))
+                        {
+                            var currentStarPower = galaxy[spaceshipRow, spaceshipCol] - '0';
+                            starPower += currentStarPower;
+                            galaxy[spaceshipRow, spaceshipCol] = 'S';
+
+                        }
+
+                        else if (char.IsLetter(galaxy[spaceshipRow, spaceshipCol]))
+                        {
+                            galaxy[spaceshipRow, spaceshipCol] = '-';
+                            FindNextBlackHole(galaxy);
+                        }
+                    }
+                    else
+                    {
+
+                        intoTheVold = true;
+                    }
+
+                    break;
+
+                case "down":
+                    galaxy[spaceshipRow, spaceshipCol] = '-';
+                    if (BoundriesCheck(galaxy, spaceshipRow + 1, spaceshipCol))
+                    {
+                        spaceshipRow++;
+
+                        if (char.IsDigit(galaxy[spaceshipRow, spaceshipCol]))
+                        {
+                            var currentStarPower = galaxy[spaceshipRow, spaceshipCol] - '0';
+                            starPower += currentStarPower;
+                            galaxy[spaceshipRow, spaceshipCol] = 'S';
+
+                        }
+
+                        else if (char.IsLetter(galaxy[spaceshipRow, spaceshipCol]))
+                        {
+                            galaxy[spaceshipRow, spaceshipCol] = '-';
+                            FindNextBlackHole(galaxy);
+                        }
+                    }
+                    else
+                    {
+                        intoTheVold = true;
+                    }
+                    break;
+
+                case "right":
+                    galaxy[spaceshipRow, spaceshipCol] = '-';
+                    if (BoundriesCheck(galaxy, spaceshipRow, spaceshipCol + 1))
+                    {
+                        spaceshipCol++;
+
+                        if (char.IsDigit(galaxy[spaceshipRow, spaceshipCol]))
+                        {
+                            var currentStarPower = galaxy[spaceshipRow, spaceshipCol] - '0';
+                            starPower += currentStarPower;
+                            galaxy[spaceshipRow, spaceshipCol] = 'S';
+
+                        }
+
+                        else if (char.IsLetter(galaxy[spaceshipRow, spaceshipCol]))
+                        {
+                            galaxy[spaceshipRow, spaceshipCol] = '-';
+                            FindNextBlackHole(galaxy);
+                        }
+                    }
+                    else
+                    {
+                        intoTheVold = true;
+                    }
+                    break;
+
+                case "left":
+                    galaxy[spaceshipRow, spaceshipCol] = '-';
+                    if (BoundriesCheck(galaxy, spaceshipRow, spaceshipCol - 1))
+                    {
+                        spaceshipCol--;
+
+                        if (char.IsDigit(galaxy[spaceshipRow, spaceshipCol]))
+                        {
+                            var currentStarPower = galaxy[spaceshipRow, spaceshipCol] - '0';
+                            starPower += currentStarPower;
+                            galaxy[spaceshipRow, spaceshipCol] = 'S';
+
+                        }
+
+                        else if (char.IsLetter(galaxy[spaceshipRow, spaceshipCol]))
+                        {
+                            galaxy[spaceshipRow, spaceshipCol] = '-';
+                            FindNextBlackHole(galaxy);
+                        }
+                    }
+                    else
+                    {
+                        intoTheVold = true;
+                    }
+                    break;
+            }
+        }
+
+        private static void FindNextBlackHole(char[,] galaxy)
+        {
+            for (int row = 0; row < galaxy.GetLength(0); row++)
+            {
+                for (int col = 0; col < galaxy.GetLength(0); col++)
+                {
+                    var current = galaxy[row, col];
+                    if (char.IsLetter(galaxy[row, col]))
+                    {
+                        spaceshipRow = row;
+                        spaceshipCol = col;
+
+                        galaxy[spaceshipRow, spaceshipCol] = 'S';
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static bool BoundriesCheck(char[,] galaxy, int row, int col)
+        {
+            if (row < 0
+                || row >= galaxy.GetLength(0)
+                || col < 0
+                || col >= galaxy.GetLength(0))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ C#-Advanced/Exam-23-June-2019/ExamProblems/SpaceshipCrafting/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpaceshipCrafting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var liquidsInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var physicalItemsInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> liquids = new Queue<int>(liquidsInput);
+            Stack<int> items = new Stack<int>(physicalItemsInput);
+
+            int glass = 0;
+            int aluminium = 0;
+            int lithium = 0;
+            int carbonFiber = 0;
+
+            while (liquids.Count > 0 && items.Count > 0)
+            {
+                var liquid = liquids.Dequeue();
+                var item = items.Pop();
+
+                var result = liquid + item;
+
+                if (result==25)
+                {
+                    glass++;
+                }
+
+                else if (result==50)
+                {
+                    aluminium++;
+                }
+
+                else if (result==75)
+                {
+                    lithium++;
+                }
+
+                else if (result==100)
+                {
+                    carbonFiber++;
+                }
+
+                else
+                {
+                    item += 3;
+                    items.Push(item);
+                }
+
+            }
+
+            if (aluminium>0 
+                && lithium>0
+                && glass>0
+                && carbonFiber>0)
+            {
+                Console.WriteLine("Wohoo! You succeeded in building the spaceship!");
+            }
+
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to build the spaceship.");
+            }
+
+            if (liquids.Count>0)
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", liquids)}");
+            }
+            else
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+
+            if (items.Count>0)
+            {
+                Console.WriteLine($"Physical items left: {string.Join(", ", items)}");
+            }
+            else
+            {
+                Console.WriteLine("Physical items left: none");
+            }
+
+            Console.WriteLine($"Aluminium: {aluminium}");
+            Console.WriteLine($"Carbon fiber: {carbonFiber}");
+            Console.WriteLine($"Glass: {glass}");
+            Console.WriteLine($"Lithium: {lithium}");
+        }
+    }
+}
