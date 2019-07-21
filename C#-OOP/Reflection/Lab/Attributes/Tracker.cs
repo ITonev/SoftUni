1b85bfa603c8698bf16a6d93a0262b0d93c9dc39
ITonev <ivan_tonev@mail.bg>
2019-07-21 18:51:07 +0300

    reflection lab

---
+++ C#-OOP/Reflection/Lab/Attributes/Tracker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        var classType = typeof(StartUp);
+
+        var methods = classType.GetMethods(BindingFlags.Instance
+                            | BindingFlags.Public
+                            | BindingFlags.Static);
+
+        foreach (var method in methods)
+        {
+            if (method.CustomAttributes.Any(x=>x.AttributeType==typeof(AuthorAttribute)))
+            {
+                var methodAtts = method.GetCustomAttributes(false);
+
+                foreach (AuthorAttribute att in methodAtts)
+                {
+                    Console.WriteLine($"{method.Name} is written by {att.Name}");
+                }
+            }
+        }
+    }
+}
+
