4ccabe1c85c0583b63c2f8e6c41787a27826c9fe
ITonev <ivan_tonev@mail.bg>
2019-08-03 10:36:23 +0300

    unit testing lab

---
+++ C#-OOP/UnitTesting/Lab/Skeleton/Contracts/ITarget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skeleton.Contracts
+{
+    public interface ITarget
+    {
+        int Health { get; }
+
+        int GiveExperience();
+
+        void TakeAttack(int attackPoints);
+
+        bool IsDead();
+
+    }
+}
