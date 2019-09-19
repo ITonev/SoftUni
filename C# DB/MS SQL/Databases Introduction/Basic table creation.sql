d3bbf2076b83d04db993766b8c354895cd60aba5
ITonev <ivan_tonev@mail.bg>
2019-09-19 21:37:51 +0300

    basic ms sql functions

--- C# DB/MS SQL/Databases Introduction/Basic table creation.sql
+++ C# DB/MS SQL/Databases Introduction/Basic table creation.sql
@@ -15,3 +15,35 @@ AccountTypeId INT FOREIGN KEY REFERENCES AccountTypes(Id),
 Balance DECIMAL(15,2) NOT NULL DEFAULT(0),
 ClientId INT FOREIGN KEY REFERENCES Clients(ID)
 )
+
+INSERT INTO Clients(FirstName, LastName) VALUES
+('Gosho', 'Ivanov'),
+('Pesho', 'Petrov'),
+('Ivan', 'Iliev'),
+('Merry', 'Ivanova')
+
+INSERT INTO AccountTypes(Name) VALUES
+('Checking'),
+('Savings')
+
+INSERT INTO Accounts(ClientId, AccountTypeId, Balance) VALUES
+(1, 1, 175),
+(2, 1, 275.56),
+(3, 1, 138.01),
+(4, 1, 40.30),
+(4, 2, 375.50)
+
+GO
+CREATE FUNCTION f_CalculateTotalBalance (@ClientID INT)
+RETURNS DECIMAL(15, 2)
+BEGIN
+	DECLARE @result AS DECIMAL(15, 2) = (
+	SELECT SUM(Balance)
+	FROM Accounts WHERE ClientId = @ClientId
+	)
+RETURN @result
+END
+GO
+
+SELECT dbo.f_CalculateTotalBalance(1) AS Balance
+
