56364d410d27d31e7702108ed01539096694d59c
ITonev <ivan_tonev@mail.bg>
2019-09-19 22:22:36 +0300

    ms sql funcs, triggers, procedures

--- C# DB/MS SQL/Databases Introduction/Basic table creation.sql
+++ C# DB/MS SQL/Databases Introduction/Basic table creation.sql
@@ -47,3 +47,74 @@ GO
 
 SELECT dbo.f_CalculateTotalBalance(1) AS Balance
 
+--Procedure
+GO
+CREATE PROC p_AddAccount @ClientId INT, @AccountTypeId INT AS
+INSERT INTO Accounts(ClientId, AccountTypeId)
+VALUES (@ClientId, @AccountTypeId)
+GO
+
+p_AddAccount 2,2
+
+SELECT * FROM Accounts
+
+GO
+CREATE PROC p_Deposit @AccountId INT, @Amount DECIMAL(15,2) AS
+UPDATE Accounts
+SET Balance+= @Amount
+WHERE ID = @AccountId
+GO
+
+p_Deposit 8, 123.12
+
+GO
+CREATE PROC p_Withdraw @AccountId INT, @Amount DECIMAL(15,2) AS
+BEGIN
+	DECLARE @OldBalance DECIMAL(15,2)
+	SELECT @OldBalance = Balance FROM Accounts WHERE ID=@AccountId
+	IF(@OldBalance - @Amount >= 0)
+	BEGIN
+		UPDATE Accounts
+		SET Balance-=@Amount 
+		WHERE ID=@AccountId
+	END
+	ELSE
+	BEGIN
+		RAISERROR('Insufficient funds', 10,1)
+	END
+END
+GO
+
+p_Withdraw 8, 190
+
+CREATE TABLE Transactions (
+Id INT PRIMARY KEY IDENTITY(1,1),
+AccountId INT FOREIGN KEY REFERENCES Accounts(ID),
+OldBalance DECIMAL(15,2) NOT NULL,
+NewBalance DECIMAL(15,2) NOT NULL,
+Amount AS NewBalance - OldBalance,
+[DateTime] DATETIME2
+)
+
+GO
+CREATE TRIGGER tr_Transaction ON Accounts
+AFTER UPDATE
+AS
+	INSERT INTO Transactions(AccountId,OldBalance,NewBalance,[DateTime])
+	SELECT inserted.Id, deleted.Balance, inserted.Balance, GETDATE() FROM inserted
+	JOIN deleted ON inserted.Id = deleted.Id
+GO
+
+p_Deposit 1, 25.00
+GO
+
+p_Deposit 1, 40.00
+GO
+
+p_Withdraw 2, 200.00
+GO
+
+p_Deposit 4, 180.00
+GO
+
+SELECT * FROM Transactions
