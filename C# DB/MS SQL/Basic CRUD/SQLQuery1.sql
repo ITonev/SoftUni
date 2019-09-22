57882c5bb0b435b0863dd19d551ee2e3b5ed2052
ITonev <ivan_tonev@mail.bg>
2019-09-22 13:15:18 +0300

    ms sql CRUD operations

--- C# DB/MS SQL/Basic CRUD/SQLQuery1.sql
+++ C# DB/MS SQL/Basic CRUD/SQLQuery1.sql
@@ -1,2 +1,11 @@
 Use SoftUni
 
+SELECT * FROM Departments
+
+SELECT [Name] FROM Departments
+
+SELECT FirstName, MiddleName, LastName FROM Employees
+
+SELECT FirstName+'.'+LastName+'@'+'softuni.bg' AS [Full Email Address] FROM Employees AS e
+
+
