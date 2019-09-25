4091c65ceb1e5b86df6d2ee993b3900bcf2f813b
ITonev <ivan_tonev@mail.bg>
2019-09-25 23:12:36 +0300

    ms sql build in functions

--- C# DB/MS SQL/Build-In Functions/SQLQuery1.sql
+++ C# DB/MS SQL/Build-In Functions/SQLQuery1.sql
@@ -57,8 +57,13 @@ USE [Geography]
    WHERE CountryName LIKE '%a%a%a%'
 ORDER BY IsoCode
 
-SELECT * FROM CountriesRivers
-INNER JOIN Rivers
+  SELECT p.PeakName,
+	     r.RiverName,
+		 LOWER(CONCAT(p.PeakName, RIGHT(r.RiverName, LEN(r.RiverName)-1))) AS [Mix] 
+    FROM Peaks AS p, 
+	     Rivers AS r
+   WHERE RIGHT(p.PeakName,1) = LEFT(r.RiverName,1)
+ORDER BY [Mix]
 
 USE Diablo
 
