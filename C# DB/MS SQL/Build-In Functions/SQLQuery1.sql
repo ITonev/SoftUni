a52167496529ac3bf173712ffc700d4b686ff6f4
ITonev <ivan_tonev@mail.bg>
2019-09-25 22:02:14 +0300

    ms sql build in functions

--- C# DB/MS SQL/Build-In Functions/SQLQuery1.sql
+++ C# DB/MS SQL/Build-In Functions/SQLQuery1.sql
@@ -73,4 +73,24 @@ ORDER BY [Start], [Name]
     FROM Users
 ORDER BY [Email Provider], Username
 
+  SELECT Username,
+  	     IpAddress
+    FROM Users
+   WHERE IpAddress LIKE '___.1%.%.___'
+ORDER BY Username
+
+  SELECT [Name],
+	  CASE
+	   WHEN DATEPART(hour,[Start]) >= 0 AND DATEPART(hour,[Start]) < 12 THEN 'Morning'
+	   WHEN DATEPART(hour,[Start]) >= 12 AND DATEPART(hour,[Start]) < 18 THEN 'Afternoon'
+	   ELSE 'Evening'
+	   END AS [Part of the Day],
+	  CASE 
+	  WHEN Duration <= 3 THEN 'Extra Short'
+	  WHEN Duration >=4 AND Duration <= 6 THEN 'Short'
+	  WHEN Duration >= 7 THEN 'Long'
+	  ELSE 'Extra Long'
+	  END AS Duration
+    FROM Games
+ORDER BY [Name], Duration
 
