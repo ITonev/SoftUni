87a3558c6a6ea117e0d55d1c3240f32b4b1eac04
ITonev <ivan_tonev@mail.bg>
2019-09-28 21:05:33 +0300

    data aggregation

--- C# DB/MS SQL/Data Aggregation/SQLQuery1.sql
+++ C# DB/MS SQL/Data Aggregation/SQLQuery1.sql
@@ -11,4 +11,16 @@ MAX(w.MagicWandSize) AS LongestMagicWand
 FROM WizzardDeposits w
 GROUP BY w.DepositGroup
 
+SELECT TOP(2) w.DepositGroup
+FROM WizzardDeposits w
+GROUP BY w.DepositGroup
+ORDER BY AVG(w.MagicWandSize) 
+
+SELECT w.DepositGroup,
+SUM(w.DepositAmount)
+FROM WizzardDeposits w
+GROUP BY w.DepositGroup
+
+
+
 
