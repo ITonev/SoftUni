8cfa10522e8c02105d5ae7e867ae75a0f820e184
ITonev <ivan_tonev@mail.bg>
2019-09-28 21:00:08 +0300

    data aggregation

--- C# DB/MS SQL/Data Aggregation/SQLQuery1.sql
+++ C# DB/MS SQL/Data Aggregation/SQLQuery1.sql
@@ -1,2 +1,14 @@
 Use Gringotts
 
+SELECT COUNT(*) as [Count]
+FROM WizzardDeposits AS w
+
+SELECT MAX(w.MagicWandSize) AS LongestMagicWand
+FROM WizzardDeposits AS w
+
+SELECT w.DepositGroup,
+MAX(w.MagicWandSize) AS LongestMagicWand
+FROM WizzardDeposits w
+GROUP BY w.DepositGroup
+
+
