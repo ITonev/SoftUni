c6b507ca10ba71f96c74e6449fb539dba76a05b6
ITonev <ivan_tonev@mail.bg>
2019-10-03 21:20:42 +0300

    table relations

--- C# DB/MS SQL/Table Relations/SQLQuery1.sql
+++ C# DB/MS SQL/Table Relations/SQLQuery1.sql
@@ -176,9 +176,14 @@ CREATE TABLE Agenda
 	CONSTRAINT PK_Agenda PRIMARY KEY (StudentID, SubjectID)
 )
 
+USE Geography
 
-
-
+SELECT m.MountainRange, p.PeakName, p.Elevation
+FROM Peaks AS p
+JOIN Mountains AS m
+ON p.MountainId=m.Id
+WHERE m.MountainRange='Rila'
+ORDER BY p.Elevation DESC
 
 
 
