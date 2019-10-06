be16f6c67e2ff4284f000b72038a397a7e93f0c9
ITonev <ivan_tonev@mail.bg>
2019-10-06 17:52:30 +0300

    subqueries, joins and STE

--- C# DB/MS SQL/Subqueries and JOINs/SQLQuery1.sql
+++ C# DB/MS SQL/Subqueries and JOINs/SQLQuery1.sql
@@ -22,14 +22,27 @@ JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
 WHERE e.Salary > 15000
 ORDER BY e.DepartmentID 
 
+SELECT TOP 3 e.EmployeeID, e.FirstName 
+FROM Employees AS e
+FULL JOIN EmployeesProjects AS ep ON e.EmployeeID=ep.EmployeeID
+FULL JOIN Projects as p ON p.ProjectID=Ep.ProjectID
+WHERE p.ProjectID IS NULL
+ORDER BY e.EmployeeID
 
+SELECT e.FirstName, e.LastName, e.HireDate, d.[Name]
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID 
+AND d.[Name] IN ('Sales','Finance')
+WHERE e.HireDate > '1.1.1999'
+ORDER BY e.HireDate
 
-
-
-
-
-
-
+SELECT TOP 5 e.EmployeeID, e.FirstName, p.[Name] 
+FROM Employees AS e
+JOIN EmployeesProjects AS em ON e.EmployeeID=em.EmployeeID
+JOIN Projects AS p ON p.ProjectID=em.ProjectID
+AND p.StartDate > '20020813'
+AND p.EndDate IS NULL
+ORDER BY e.EmployeeID
 
 
 
