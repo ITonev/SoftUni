3f9db112fe8cf0daefdd454de01c9969235bd4e3
ITonev <ivan_tonev@mail.bg>
2019-10-06 17:43:39 +0300

    subqueries, joins and STE

--- C# DB/MS SQL/Subqueries and JOINs/SQLQuery1.sql
+++ C# DB/MS SQL/Subqueries and JOINs/SQLQuery1.sql
@@ -4,19 +4,23 @@ JOIN Addresses AS a
 ON e.AddressID = a.AddressID 
 ORDER BY e.AddressID
 
+SELECT TOP 50 e.FirstName, e.LastName, t.[Name], a.AddressText
+FROM Employees AS e
+JOIN Addresses AS a ON e.AddressID = a.AddressID
+JOIN Towns AS t ON a.TownID = t.TownID
+ORDER BY e.FirstName, e.LastName
 
+SELECT e.EmployeeID, e.FirstName, e.LastName, d.[Name]
+FROM Employees AS e
+JOIN Departments AS d 
+ON e.DepartmentID = d.DepartmentID AND d.[Name] = 'Sales'
+ORDER BY e.EmployeeID
 
-
-
-
-
-
-
-
-
-
-
-
+SELECT TOP 5 e.EmployeeID, e.FirstName, e.Salary, d.[Name]
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE e.Salary > 15000
+ORDER BY e.DepartmentID 
 
 
 
