ff4e785d184136d6b25b75f373cdc11e84cd04ae
ITonev <ivan_tonev@mail.bg>
2019-10-10 21:25:26 +0300

    functions and procedures

--- C# DB/MS SQL/Functions and Procedures/SQLQuery1.sql
+++ C# DB/MS SQL/Functions and Procedures/SQLQuery1.sql
@@ -25,10 +25,37 @@ AS
 
 GO
 
+CREATE PROC usp_GetEmployeesFromTown (@TownName NVARCHAR(30))
+AS
+	SELECT e.FirstName, e.LastName
+	FROM Employees AS e
+	JOIN Addresses AS a ON a.AddressID = e.AddressID
+	JOIN Towns AS t ON t.TownID=a.TownID
+	WHERE t.[Name] = @TownName
 
+GO
 
+CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+RETURNS VARCHAR(20)
+AS
+	BEGIN
+	DECLARE @salaryLevel VARCHAR(20)
+		IF(@salary < 30000)
+		BEGIN
+			SET @salaryLevel = 'Low'
+		END
+		ELSE IF(@salary >= 30000 AND @salary <= 50000)
+		BEGIN
+			SET @salaryLevel = 'Average'
+		END
+		ELSE 
+		BEGIN
+			SET @salaryLevel = 'High'
+		END
+	RETURN @salaryLevel
+END
 
-
+GO
 
 
 
