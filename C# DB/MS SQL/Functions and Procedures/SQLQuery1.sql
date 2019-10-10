46ddf64a355ceb3bb574d42e7d0af3c15d3f9dac
ITonev <ivan_tonev@mail.bg>
2019-10-10 21:07:12 +0300

    functions and procedures

--- C# DB/MS SQL/Functions and Procedures/SQLQuery1.sql
+++ C# DB/MS SQL/Functions and Procedures/SQLQuery1.sql
@@ -5,15 +5,25 @@ CREATE PROC usp_GetEmployeesSalaryAbove35000
 AS
 	SELECT FirstName, LastName 
 	FROM Employees
-	WHERE 
+	WHERE Salary > 35000
 
+GO
 
+CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber (@Number DECIMAL(18,4))
+AS
+	SELECT FirstName, LastName
+	FROM Employees
+	WHERE Salary >= @Number
 
+GO
 
+CREATE PROC usp_GetTownsStartingWith (@Parameter NVARCHAR(10)) 
+AS
+	SELECT [Name] As Town
+	FROM Towns
+	WHERE [Name] LIKE @Parameter + '%'
 
-
-
-
+GO
 
 
 
