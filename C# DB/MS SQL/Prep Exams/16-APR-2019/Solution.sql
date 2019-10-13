2ed77e462ed971ac757214806ea56e1d8d853601
ITonev <ivan_tonev@mail.bg>
2019-10-13 17:16:49 +0300

    prep exam

--- C# DB/MS SQL/Prep Exams/16-APR-2019/Solution.sql
+++ C# DB/MS SQL/Prep Exams/16-APR-2019/Solution.sql
@@ -10,22 +10,60 @@ Seats INT NOT NULL,
 )
 
 CREATE TABLE Flights (
-
+Id INT PRIMARY KEY IDENTITY,
+DepartureTime DATETIME2,
+ArrivalTime DATETIME2,
+Origin VARCHAR(50) NOT NULL,
+Destination VARCHAR(50) NOT NULL,
+PlaneId INT FOREIGN KEY REFERENCES Planes(Id) NOT NULL
 )
 
+CREATE TABLE Passengers(
+Id INT PRIMARY KEY IDENTITY,
+FirstName VARCHAR(30) NOT NULL,
+LastName VARCHAR(30) NOT NULL,
+Age INT NOT NULL,
+[Address] VARCHAR(30) NOT NULL,
+PassportId CHAR(11) NOT NULL
+)
 
+CREATE TABLE LuggageTypes(
+Id INT PRIMARY KEY IDENTITY,
+[Type] VARCHAR(30) NOT NULL,
+)
 
+CREATE TABLE Luggages(
+Id INT PRIMARY KEY IDENTITY,
+LuggageTypeId INT FOREIGN KEY REFERENCES LuggageTypes(Id) NOT NULL,
+PassengerId INT FOREIGN KEY REFERENCES Passengers(Id) NOT NULL
+)
 
+CREATE TABLE Tickets(
+Id INT PRIMARY KEY IDENTITY,
+PassengerId INT FOREIGN KEY REFERENCES Passengers(Id) NOT NULL,
+FlightId INT FOREIGN KEY REFERENCES Flights(Id) NOT NULL,
+LuggageId INT FOREIGN KEY REFERENCES Luggages(Id) NOT NULL,
+Price DECIMAL(18,2) NOT NULL
+)
 
+INSERT INTO Planes([Name],Seats,[Range]) VALUES
+('Airbus 336', 112, 5132),
+('Airbus 330', 432, 5325),
+('Boeing 369',231,2355),
+('Stelt 297',254, 2143),
+('Boeing 338',165, 5111),
+('Airbus 558',387, 1342),
+('Boeing 128',345, 5541)
 
+INSERT INTO LuggageTypes([Type]) VALUES
+('Crossbody Bag'),
+('School Backpack'),
+('Shoulder Bag')
 
-
-
-
-
-
-
-
+SELECT * FROM Flights AS f
+JOIN Tickets AS t ON t.FlightId = f.Id
+WHERE f.Destination = 'Carlsbad'
+UPDATE Tickets SET Price *=1.13
 
 
 
