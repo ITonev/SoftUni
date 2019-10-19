7681d74e771fb3001f0c902e3ec23d1a3d77b04b
ITonev <ivan_tonev@mail.bg>
2019-10-19 17:08:29 +0300

    prep exam

--- C# DB/MS SQL/Prep Exams/17-FEB-2019/SQLQuery1.sql
+++ C# DB/MS SQL/Prep Exams/17-FEB-2019/SQLQuery1.sql
@@ -180,7 +180,29 @@ END
 
 GO
 
+CREATE PROCEDURE usp_ExcludeFromSchool(@StudentId INT)
+AS
+ BEGIN
+	DECLARE @StudentIdExist INT = (SELECT Id FROM Students WHERE Id = @studentId)
 
+	IF(@StudentIdExist IS NULL)
+	  BEGIN
+	   RAISERROR('This school has no student with the provided id!', 16,1)
+	  END
+
+	DELETE FROM StudentsTeachers
+	WHERE StudentId = @StudentId
+
+	DELETE FROM StudentsExams
+	WHERE StudentId = @StudentId
+
+	DELETE FROM StudentsSubjects
+	WHERE StudentId = @StudentId
+
+	DELETE FROM Students
+	WHERE Id = @StudentId
+	
+ END
 
 
 
