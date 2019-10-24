2d23d366d53a02bff053445444d29293e233a80c
ITonev <ivan_tonev@mail.bg>
2019-10-24 22:54:30 +0300

    DB APPS

---
+++ C# DB/Entity Framework Core/DB APPS Introduction/Solution/ConsoleApp/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp
+{
+    public class StartUp
+    {
+        private static string connectionString = @"Server=PRETORIAN\SQLEXPRESS;Database=master;Integrated Security=true";
+        public static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            using (connection)
+            {
+            }
+        }
+    }
+}
