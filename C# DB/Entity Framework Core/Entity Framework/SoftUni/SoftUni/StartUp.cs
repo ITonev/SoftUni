76e2425bc61ab2017f33ede928cf9552769ddfb3
ITonev <ivan_tonev@mail.bg>
2019-10-31 22:25:45 +0200

    entity framework

---
+++ C# DB/Entity Framework Core/Entity Framework/SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using SoftUni.Data;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                System.Console.WriteLine(GetEmployeesFullInformation(db));
+            }
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context
+                            .Employees
+                            .Select(t => new
+                            {
+                                t.EmployeeId,
+                                t.FirstName,
+                                t.LastName,
+                                t.MiddleName,
+                                t.JobTitle,
+                                t.Salary
+                            })
+                            .OrderBy(t => t.EmployeeId)
+                            .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var emp in employees)
+            {
+                sb.AppendLine($"{emp.FirstName} {emp.LastName} {emp.MiddleName} {emp.JobTitle} {emp.Salary:F2}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
