aa2cdd6d7f6d1a8104c13a49daba9675837d2759
ITonev <ivan_tonev@mail.bg>
2019-11-05 22:27:59 +0200

    code first

---
+++ C# DB/Entity Framework Core/CodeFirst/P01_HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data
+{
+   public class HospitalContext : DbContext
+    {
+
+    }
+}
